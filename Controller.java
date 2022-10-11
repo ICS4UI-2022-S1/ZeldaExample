55c641b232854ac1bdbfe0f0e19aeb193b045a6f
Mr Lamont <adam_lamont@wrdsb.ca>
2022-10-11 08:16:07 -0400

    Added Controller and Scene

--- Controller.java
+++ Controller.java
@@ -14,6 +14,46 @@ import java.util.Scanner;
  * @author lamonta
  */
 public class Controller {
-   
+   private int currentRow;
+   private int currentCol;
+   private MainScreen theView;
+   private Scene currentScene;
+   private Scene[] map;
+
+   public Controller(String filename, MainScreen view){
+    this.theView = view;
+    // read in from a file
+    Scanner input = null;
+    try{
+        input = new Scanner(new File(filename));
+    }catch(Exception e){
+        // print the error
+        e.printStackTrace();
+    }
+    // file is ready for reading
+    int numRows = input.nextInt();
+    // move down a row
+    input.nextLine();
+    int numCols = input.nextInt();
+    // move down
+    this.currentRow = input.nextInt();
+    // move down
+    this.currentCol = input.nextInt();
+    // move down
+    input.nextLine();
+    // make the map storage
+    this.map = new Scene[numRows*numCols];
+    // loop through the rest of the data
+    for(int i = 0; i < this.map.length; i++){
+        String imageName = input.next();
+        boolean up = input.nextBoolean();
+        boolean right = input.nextBoolean();
+        boolean down = input.nextBoolean();
+        boolean left = input.nextBoolean();
+        String description = input.nextLine();
+        this.map[i] = new Scene(imageName, up, right, down, left, description);
+    }
+
+   }
     
 }
--- Scene.java
+++ Scene.java
@@ -15,7 +15,71 @@ import javax.imageio.ImageIO;
  * @author lamonta
  */
 public class Scene {
-   
+   private BufferedImage picture;
+   private int row;
+   private int col;
+   private boolean canMoveUp;
+   private boolean canMoveRight;
+   private boolean canMoveDown;
+   private boolean canMoveLeft;
+   private String description;
+
+   public Scene(String imageName, boolean canMoveUp,boolean canMoveRight, boolean canMoveDown, boolean canMoveLeft, String description){
+    this.canMoveUp = canMoveUp;
+    this.canMoveRight = canMoveRight;
+    this.canMoveDown = canMoveDown;
+    this.canMoveLeft = canMoveLeft;
+    this.description = description;
+    // load the picture
+    this.picture = null;
+    try{
+        // try to load picture from the images folder
+        this.picture = ImageIO.read(new File("images/" + imageName));
+    }catch(Exception e){
+        // print out the error
+        e.printStackTrace();
+    }
+    // split up the name to get row and col
+    String[] data = imageName.split("-");
+    // remove the .png part
+    data[3] = data[3].replace(".png","");
+    // convert the Strings to ints to store
+    this.row = Integer.parseInt(data[1]);
+    this.col = Integer.parseInt(data[3]);
+
+   }
+
+   public BufferedImage getImage(){
+    return this.picture;
+   }
+
+   public int getRow(){
+    return this.row;
+   }
+
+   public int getCol(){
+    return this.col;
+   }
+
+   public boolean canMoveUp(){
+    return this.canMoveUp;
+   }
+
+   public boolean canMoveRight(){
+    return this.canMoveRight;
+   }
+
+   public boolean canMoveDown(){
+    return this.canMoveDown;
+   }
+
+   public boolean canMoveLeft(){
+    return this.canMoveLeft;
+   }
+
+   public String getDescription(){
+    return this.description;
+   }
     
     
 }
